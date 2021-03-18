80887ad6ed83097e2e9c52bd6cd5eacf63d53ba6
Floatyy1998 <konrad.dinges@googlemail.com>
2021-03-18 01:14:35 +0100

    added file input... muss backend getestet werden

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
-const axios = require("axios");
+import axios from "axios";
 
-axios.defaults.xsrfCookieName = 'csrftoken'
-axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 class App extends Component {
   state = {
-    todos: []
+    todos: [],
+    selectedFile: null
+
   };
 
+    fileChangedHandler = event => {
+      this.setState(prevState=>({todos:prevState.todos, selectedFile: event.target.files[0] }))
+    }
 
-componentDidMount(){
-  axios.get("/api/todos/")
-  .then(res=> this.setState({ todos: res.data}))
-  .catch(err=>console.log(err));
-};
+    uploadHandler = () => {
+      const formData = new FormData()
+      formData.append(
+        'myFile',
+        this.state.selectedFile,
+        this.state.selectedFile.name
+      )
+      axios.post('UNSERE DOMAIN', formData, {
+        onUploadProgress: progressEvent => {
+          console.log(progressEvent.loaded / progressEvent.total)
+        }
+      })
+    }
+  
+  componentDidMount() {
+    axios
+      .get("/api/todos/")
+      .then((res) => this.setState({ todos: res.data }))
+      .catch((err) => console.log(err));
+  }
 
-render() {
-  return (
-    <div>
-      {this.state.todos.map(item=>(
-        <div key={item.id}>
-          <h1>{item.title}</h1>
-          </div>
-      ))}
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <input type="file" onChange={this.fileChangedHandler}></input>
+        <button onClick={this.uploadHandler}>Hochladen oder so!</button>
+        <div>
+          {this.state.todos.map((item) => (
+            <div key={item.id}>
+              <h1>{item.title}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
