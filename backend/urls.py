bf4aed0005ad432956d87ae229268da4afc47932
janS95 <jan.senftleben@web.de>
2021-03-19 20:51:48 +0100

    'sw.js'

--- backend/urls.py
+++ backend/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),         
     path('api/', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('service-worker.js', TemplateView.as_view(template_name='service-worker.js')),
 ]
