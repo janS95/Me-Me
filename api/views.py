77251a27f4b120a53598676bd9be996907bb0b89
Floatyy1998 <konrad.dinges@googlemail.com>
2021-03-20 06:05:33 +0100

    added emotion detection

--- api/views.py
+++ api/views.py
@@ -4,6 +4,11 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from .serializers import ImageSerializer      
 from .models import Image
 from django.http import HttpResponse, HttpResponseNotFound
+from fer import FER
+import cv2
+import os
+
+
 
 
 class ImageView(generics.ListCreateAPIView):
@@ -12,6 +17,14 @@ class ImageView(generics.ListCreateAPIView):
 
     def post(self, request, format=None):
         new_image = ImageSerializer(data=request.data)
-        if new_image.is_valid():        
-            return HttpResponse('Hässlich')
+        if new_image.is_valid(): 
+            try:
+                new_image.save()
+                img = cv2.imread('.\\api\\images\\'+str(request.data['image']))
+                detector = FER()
+                res=detector.top_emotion(img)[0]  
+                os.remove('.\\api\\images\\'+str(request.data['image'])) 
+                return HttpResponse(res)
+            except IndexError:
+                return HttpResponse('Kein Gesicht erkannt')
         return HttpResponse('Fick dich')
